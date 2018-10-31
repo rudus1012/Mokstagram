8e93b5169bddbf0af30a7d9eb9931a9d9f3ac985
entect <rudus1012@naver.com>
2018-10-31 15:37:57 +0900

    Fixed URL type:

---
+++ nomadgram/images/models.py
@@ -0,0 +1,61 @@
+from django.db import models
+from django.utils.encoding import python_2_unicode_compatible
+from nomadgram.users import models as user_models
+from taggit.managers import TaggableManager
+
+
+# Create your models here.
+
+
+
+
+class TimeStampedModel(models.Model):
+
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        abstract = True 
+
+
+
+class Image(TimeStampedModel):
+
+    
+    file = models.ImageField()
+    location = models.CharField(max_length=140)
+    caption = models.TextField()
+    creator = models.ForeignKey(user_models.User, on_delete=models.PROTECT,null=True,related_name='images')
+    tags = TaggableManager()
+
+    @property
+    def like_count(self):
+        return self.likes.all().count()
+
+    @property
+    def comment_count(self):
+        return self.comments.all().count()
+
+    def __str__(self):
+        return '{} - {}'.format(self.location, self.caption) 
+
+        class Meta:
+            ordering = ['-created_at']
+
+class Comment(TimeStampedModel):
+
+    message = models.TextField()
+    creator = models.ForeignKey(user_models.User, on_delete=models.PROTECT,null=True)
+    image = models.ForeignKey(Image, on_delete=models.PROTECT,null=True,related_name='comments')
+
+    def __str__(self):
+        return self.message
+
+
+class Like(TimeStampedModel):
+
+    creator = models.ForeignKey(user_models.User, on_delete=models.PROTECT,null=True)
+    image = models.ForeignKey(Image, on_delete=models.PROTECT,null=True,related_name='likes')
+
+    def __str__(self):
+        return 'User:{} - Image Caption: {}'.format(self.creator.username, self.image.caption)
